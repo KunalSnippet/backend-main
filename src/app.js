04b1bcccc6b305a1e090f435df28e3bceb571f30
KunalSnippet <kunalbgr23@gmail.com>
2026-06-06 11:59:56 +0530

    adding error response files and db files

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from " cors";
+import cookieParser from "cookie-parser";
+
+const app = express();
+
+app.use(cors({
+  origin: "CORS_ORIGIN",
+  credentials: true
+}));
+
+app.use(express.json({
+  limit: "10mb"
+}));
+
+app.use(express.urlencoded({
+  limit: "10mb"
+}));
+
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
+export {app}
---
+++ src/index.js
@@ -0,0 +1,42 @@
+import dotenv from "dotenv";
+import connectDb from "./db/index.js";
+import app from "./app.js";
+
+dotenv.config({
+  path: "./env",
+});
+
+connectDb()
+.then(() => {
+  app.listen(process.env.PORT || 8000, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+  })
+})
+.catch((error) => {
+  console.log(`Error connecting to database ${error}`);
+})
+
+
+
+
+
+
+
+
+
+// const app = express();
+// (async () => {
+//   try{
+//     await mongoose.connect(` ${process.env.MONGODB_URI}/${DB_NAME}`);
+//     app.on("error", (error) => {
+//       console.log(`Errror occured in app ${error}`);
+//     })
+
+//     app.listen(process.env.PORT, () => {
+//       console.log(`Server is ruunning on the port ${process.env.PORT}`);
+//     })
+//   }
+//   catch(error){
+//     consoole.error(`Error occured ${error}`);
+//   }
+// })()
